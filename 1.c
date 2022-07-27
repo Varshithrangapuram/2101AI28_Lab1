16d83eb303ee7ab660dcb365c0727d290702b6d1
Varshithrangapuram <110088029+Varshithrangapuram@users.noreply.github.com>
2022-07-27 13:46:55 +0530

    asking user to input the number for each operation

--- 1.c
+++ 1.c
@@ -5,3 +5,4 @@ int c;
 printf("enter two numbers\n");
 scanf("%f %f",&a,&b);
 printf("enter the operation you wanted to perform between two numbers\n");
+printf("press 0 for addition operation\n press 1 for subtraction operation\n press 2 for multiplication operation\n press 3 for division operation\n");
