b800069e576fe409236bde548c83dc9b49508cb4
Varshithrangapuram <110088029+Varshithrangapuram@users.noreply.github.com>
2022-07-27 21:44:15 +0530

    addition of selection sort algorithm

--- 02.c
+++ 02.c
@@ -7,3 +7,20 @@ void swap(int *xp, int *yp)
     *xp = *yp;
     *yp = temp;
 }
+void selectionSort(int arr[], int n)
+{
+    int i, j, min_idx;
+
+
+    for (i = 0; i < n-1; i++)
+    {
+
+        min_idx = i;
+        for (j = i+1; j < n; j++)
+          if (arr[j] < arr[min_idx])
+            min_idx = j;
+
+
+        swap(&arr[min_idx], &arr[i]);
+    }
+}
