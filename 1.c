c177315243893b1239af5fb7cb72eacfdc2a3601
Varshithrangapuram <110088029+Varshithrangapuram@users.noreply.github.com>
2022-07-27 13:48:15 +0530

    performing addition operation

--- 1.c
+++ 1.c
@@ -6,3 +6,6 @@ printf("enter two numbers\n");
 scanf("%f %f",&a,&b);
 printf("enter the operation you wanted to perform between two numbers\n");
 printf("press 0 for addition operation\n press 1 for subtraction operation\n press 2 for multiplication operation\n press 3 for division operation\n");
+scanf("%d",&c);
+if(c==0)
+printf("addition between a and b gives %f\n",a+b);
